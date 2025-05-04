3d217d72b5fadc84934a960f9c1657d84578beba
RomaF41 <106123923+rmoncada21@users.noreply.github.com>
2025-05-03 23:53:01 -0600

    Add huffman_base.py (file with huffman code) && bin and txt files to test huffman algorithm. Command to compile python3 huffman_base.py -i test_file

---
+++ Teoria_de_la_informacion/algoritmo_huffman/huffman_base.py
@@ -0,0 +1,134 @@
+import os
+import sys
+import getopt
+import csv
+import shannon_functions as shannon_compute
+
+
+# Parametros de entrada y ayuda:
+file_full_path = ""
+file_split_path = [];
+def myfunc(argv):
+    global file_full_path, file_split_path
+    arg_output = ""
+    arg_user = ""
+    arg_help = "{0} -i <input>".format(argv[0])
+    
+    try:
+        opts, args = getopt.getopt(argv[1:], "hi:", ["help", "input="])
+    except:
+        print(arg_help)
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(arg_help)  
+            sys.exit(2)
+        elif opt in ("-i", "--input"):
+            file_full_path = arg
+            file_split_path = os.path.normpath(file_full_path)
+            file_split_path = os.path.split(file_split_path)
+
+
+if __name__ == "__main__":
+    myfunc(sys.argv)
+
+
+file_huffman_comprimido = file_full_path+".huffman"
+ruta_diccionario = file_full_path+".diccionario.csv"
+recovered_path = os.path.join(file_split_path[0], "recovered_"+file_split_path[1]);
+
+#-----------------------------------------------------
+# Algoritmo de compresión de huffman
+#-----------------------------------------------------
+
+#Apertura y lectura del archivo
+string=[];
+with open(file_full_path, "rb") as f:
+    while (byte := f.read(1)):
+        # Do stuff with byte.
+        int_val = int.from_bytes(byte, "big")
+        string.append(int_val)
+
+# print(f"{string}");
+
+# Árbol binario
+class NodeTree(object):
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+    def children(self):
+        return (self.left, self.right)
+    def nodes(self):
+        return (self.left, self.right)
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+def insert_in_tree(raiz, ruta, valor):
+    if(len(ruta)==1):
+        if(ruta=='0'):
+            raiz.left = valor;
+        else:
+            raiz.right = valor;
+    else:
+        if(ruta[0]=='0'):
+            #if type(raiz.left) is int:
+            if(raiz.left==None):
+                raiz.left = NodeTree(None,None);
+            ruta = ruta[1:];
+            insert_in_tree(raiz.left,ruta,valor);
+        else:
+            #if type(raiz.right) is int:
+            if(raiz.right==None):
+                raiz.right = NodeTree(None,None);
+            ruta = ruta[1:];
+            insert_in_tree(raiz.right,ruta,valor);
+
+
+# Función principal del algoritmo de Huffman
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is int:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+    
+
+# calculo de frecuencias y probabilidades
+prob_unit = 1/len(string) # string contiene todos los caracteres repetidos
+freq = {} # diccionario
+for c in string:
+    if c in freq:
+        freq[c] += prob_unit
+    else:
+        freq[c] = prob_unit
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True) # se convierte a lista de tuplas
+# print(f"freq: {freq}")
+nodes = freq
+# print(f"Nodos: {nodes}")
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+    #print(nodes)
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+print(' Char | Huffman code ')
+print('----------------------')
+for (char, frequency) in freq:
+    print(' %-4r |%12s' % (char, huffmanCode[char]))
+
+
+################################## Implementaciones
+li_huffman_code = {} # contiene (clave,longitud del código)
+li_huffman_code = shannon_compute.longitud_huffman_code(freq, huffmanCode)
+HX = shannon_compute.entropia_fuente(freq) # entropia de la fuente
+##################################
