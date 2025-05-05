976493695152ab55d23a2b362d72405d27f1603c
RomaF41 <106123923+rmoncada21@users.noreply.github.com>
2025-05-04 20:41:41 -0600

    Update huffman_base.py && shannon_functions.py

--- Teoria_de_la_informacion/algoritmo_huffman/shannon_functions.py
+++ Teoria_de_la_informacion/algoritmo_huffman/shannon_functions.py
@@ -3,18 +3,19 @@ from math import log2
 # Cálculo de la entropia de la fuente
 def entropia_fuente(freq):
     HX = 0;
-    print("Entropia de la fuente");
+    # print("Entropia de la fuente");
     for clave, valor_pi in freq: # probabilidad del simbolo i
         # print(f"{valor_pi}");
         HX += -(valor_pi*log2(valor_pi))
     
-    print(f"H(X) = {HX}")
+    # print(f"H(X) = {HX}")
     return HX
 
 # Cálculo de la longitud original L_og
 def longitud_original(freq):
-    L_og = log2(len(freq))
-    print(f"Longitud original: {L_og}")
+    N=len(freq)
+    L_og = log2(N)
+    # print(f"Longitud original: {L_og}")
     return L_og
 
 # Cálculo de la longitud promedio de los códigos generados por huffman
@@ -25,27 +26,27 @@ def longitud_promedio_huffman_code(freq, huffman_code):
         # print(f"pi: {valor_pi}") # pi
         # print(f"li: {(len(huffman_code[clave]))}") # li
         longitud += valor_pi * len(huffman_code[clave])
-    print(f"Longitud promedio: {longitud}")
+    # print(f"Longitud promedio: {longitud}")
 
     return longitud
 
 # Cálculo de la varianza
-def varianza_huffman_code(freq, huffman_code, longitud_promedio):
+def varianza_huffman_code(freq, huffman_code, longitud_huffman_code):
     var = 0
     for clave, valor_pi in freq:
-        var += valor_pi * (len(huffman_code[clave]) - longitud_promedio) ** 2
-    print(f"varianza = {var}")
+        var += valor_pi * (len(huffman_code[clave]) - longitud_huffman_code) ** 2
+    # print(f"varianza = {var}")
 
     return var
 
 # Eficiencia original
-def eficiencia_original(HX, L_of):
-    eff_og = HX / L_of
-    print(f"Eficiencia original: {eff_og}")
-    return eff_og
+def eficiencia_original(HX, L_og):
+    if L_og == 0:
+        return 0.0 
+    return (HX/L_og)*100
 
-# Eficiencia de huffman
+# Eficiencia de Huffman
 def eficiencia_huffman(HX, L_huff):
-    eff_huff = HX / L_huff
-    print(f"Eficiencia original: {eff_huff}")
-    return eff_huff
+    if L_huff == 0:
+        return 0.0
+    return (HX/L_huff)*100
